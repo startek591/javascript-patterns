6ff90169fcb16d3adb0040331f914cb744b1d59d
Jonathan Fambro <startek591@gmail.com>
2023-11-21 11:24:16 -0500

    Iterator going backwards

--- iteratorpattern.js
+++ iteratorpattern.js
@@ -2,15 +2,15 @@ const items = [1, 2, "devsage", false, 1.24];
 
 function Iterator(items) {
   this.items = items;
-  this.index = 0;
+  this.index = items.length - 1;
 }
 
 Iterator.prototype = {
   hasNext: function () {
-    return this.index < this.items.length;
+    return this.index >= 0;
   },
   next: function () {
-    return this.items[this.index++];
+    return this.items[this.index--];
   },
 };
 
